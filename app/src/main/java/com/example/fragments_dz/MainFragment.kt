b20711794849efcb70481adbd258b93101ad4eb4
os <osss151095@gmail.com>
2023-04-15 15:41:53 +0300

    -исправил ошибку навигации между фрагментами, добавил префиксы в TextView, ограничил поля ввода одной строкой и 30 символами

--- app/src/main/java/com/example/fragments_dz/MainFragment.kt
+++ app/src/main/java/com/example/fragments_dz/MainFragment.kt
@@ -7,39 +7,29 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 
-
-class MainFragment: Fragment(R.layout.main_fragment) {
-   private lateinit var etFirstName:EditText
-   private lateinit var etSecondName:EditText
-   private lateinit var etEmail:EditText
-   private lateinit var btnGoToSecondActivity:Button
+class MainFragment : Fragment(R.layout.main_fragment) {
+    private lateinit var etFirstName: EditText
+    private lateinit var etSecondName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var btnGoToSecondScreen: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         etFirstName = view.findViewById(R.id.TextInputEditTextFirstName)
         etSecondName = view.findViewById(R.id.textInputEditTextSecondName)
         etEmail = view.findViewById(R.id.textInputEditTextEmail)
-        btnGoToSecondActivity = view.findViewById(R.id.btnGoToSecondFragment)
+        btnGoToSecondScreen = view.findViewById(R.id.btnGoToSecondFragment)
 
-        if(arguments?.getBoolean(CLEAR_BUTTON_PRESSED) == true){
-            etFirstName.setText("")
-            etSecondName.setText("")
-            etEmail.setText("")
-
-        }
-
-        btnGoToSecondActivity.setOnClickListener {
-            val bundle = Bundle().apply {
-                putString(FIRST_NAME_KEY,etFirstName.text.toString())
-                putString(SECOND_NAME_KEY,etSecondName.text.toString())
-                putString(EMAIL_KEY,etEmail.text.toString())
-
-            }
-            val secondFragment = SecondFragment().apply { arguments = bundle }
+        btnGoToSecondScreen.setOnClickListener {
+            val secondFragment = SecondFragment.newInstance(
+                etFirstName.text.toString(),
+                etSecondName.text.toString(),
+                etEmail.text.toString()
+            )
             requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container_view, secondFragment).addToBackStack(null)
-                .commit()
+                .replace(R.id.fragment_container_view, secondFragment)
+                .addToBackStack("main_to_second").commit()
+
         }
     }
 }
